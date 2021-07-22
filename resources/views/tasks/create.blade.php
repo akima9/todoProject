0fa6d53de0926dec181fef24f41ae8a694bb9afc
kimgy <akima9a@gmail.com>
2021-07-22 18:00:08 +0900

    input date

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -33,7 +33,7 @@
                 <label for="start" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                     시작일
                 </label>
-                <input type="text" name="start" id="start" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                <input type="date" name="start" id="start" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
@@ -41,7 +41,7 @@
                 <label for="end" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                     종료일
                 </label>
-                <input type="text" name="end" id="end" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                <input type="date" name="end" id="end" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
             </div>
         </div>
         <div class="text-center">
