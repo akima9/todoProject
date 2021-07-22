086b368cf8bf191f676321fd7ac8b8e454f03109
kimgy <akima9a@gmail.com>
2021-07-22 13:39:57 +0900

    fullcalendar events 작업

--- resources/views/tasks/calendar.blade.php
+++ resources/views/tasks/calendar.blade.php
@@ -2,28 +2,19 @@
 
 @section('container')
     <div id='calendar'></div>
-    {{-- @foreach ($tasks as $task)
-        <div class="block border border-blue-500 rounded py-2 px-4 hover:bg-blue-100 my-5">
-            <a href="{{ route('task.show', ['id' => $task['id']]) }}">
-                <div>
-                    <span class="font-extrabold text-2xl">{{ $task['title'] }}</span>
-                    <span class="text-sm float-right">{{ $task['updated_at'] }}</span>
-                </div>
-                <p class="p-5">{{ $task['contents'] }}</p>
-            </a>
-        </div>
-    @endforeach --}}
 @endsection
 
 <script>
-    function getTasks(){
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', '{{route('task.list')}}', true);
-        xhr.send();
-        xhr.onload = function() {
-            if(xhr.status == 200) {
-                return xhr.responseText;
-            }
+    var tasks = [];
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '{{route('task.list')}}', true);
+    xhr.responseType='json';
+    xhr.send();
+    xhr.onload = function() {
+        if(xhr.status == 200) {
+            xhr.response.forEach(element => {
+                tasks.push(element);
+            });
         }
     }
 
@@ -34,9 +25,11 @@
             initialView: 'dayGridMonth',
             selectable: true,
             editable: true,
-            eventSources: [{
-                events: getTasks()
-            }]
+            events: function(){
+                tasks.forEach(element => {
+                    return element;
+                });
+            }
             // [
             //     {
             //         title: 'test',
