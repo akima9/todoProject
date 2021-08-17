49fdcdef637f25840dcc64948fb846eeddd19601
kimgy <akima9a@gmail.com>
2021-08-17 17:08:47 +0900

    이메일 인증 재요청 페이지 수정

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">Email 확인</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            새 확인 링크가 이메일 주소로 전송되었습니다.
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    이메일에서 확인 링크를 클릭해주세요.<br>
+                    이메일을 받지 못한 경우,
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">[이메일 재발송]</button>을 클릭해주세요.
                     </form>
                 </div>
             </div>
